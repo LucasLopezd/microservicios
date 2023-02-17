0b983c347f95c78139b2dce79eb13092eb0e0455
Lucas López <lucaslopezd@hotmail.com>
2023-02-17 18:15:19 -0300

    Agregado Seguridad/Autenticador y Filtro Jwt en Gateway

--- microservicio-api-gateway/src/main/java/com/helipagos/microservicioapigateway/seguridad/SpringSeguridad.java
+++ microservicio-api-gateway/src/main/java/com/helipagos/microservicioapigateway/seguridad/SpringSeguridad.java
@@ -1,29 +1,34 @@
 package com.helipagos.microservicioapigateway.seguridad;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
 public class SpringSeguridad {
 
+    @Autowired
+    private AutenticadorJwtFiltro filtro;
+
     @Bean
     public SecurityWebFilterChain configuracion(ServerHttpSecurity http){
         return http
                 .authorizeExchange()
-                .pathMatchers("/**").permitAll()
                 .pathMatchers("/oauth/**").permitAll()
                 .pathMatchers(HttpMethod.GET,
-                        "/api/pagos**", "/api/transacciones", "/api/transacciones/{id})").hasAnyRole("ADMIN", "CLIENTE")
-                .pathMatchers(HttpMethod.POST, "/api/transacciones").hasRole("CLIENTE")
+                        "/api/pagos**", "/api/transacciones**").hasAnyRole("ADMIN", "CLIENTE")
+                .pathMatchers(HttpMethod.POST, "/api/transacciones/**").hasRole("CLIENTE")
                 .pathMatchers(HttpMethod.POST, "/api/pagos").hasRole("ADMIN")
                 .pathMatchers(HttpMethod.PUT, "/api/pagos").hasRole("ADMIN")
                 .pathMatchers(HttpMethod.DELETE,"/api/pagos/{id}").hasRole("ADMIN")
                 .anyExchange()
                 .authenticated()
                 .and()
+                .addFilterAt(filtro, SecurityWebFiltersOrder.AUTHENTICATION)
                 .csrf().disable()
                 .build();
     }
