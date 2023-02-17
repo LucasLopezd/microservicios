b0cbd2e9ddadc7f0f6fd1e4a83f832a3453299e9
Lucas López <lucaslopezd@hotmail.com>
2023-02-17 10:00:12 -0300

    Borrado de imports innecesarios

--- microservicio-oauth/src/main/java/com/helipagos/microserviciooauth/seguridad/AuthorizacionConfiguracion.java
+++ microservicio-oauth/src/main/java/com/helipagos/microserviciooauth/seguridad/AuthorizacionConfiguracion.java
@@ -1,13 +1,10 @@
 package com.helipagos.microserviciooauth.seguridad;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.jaas.AuthorityGranter;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
